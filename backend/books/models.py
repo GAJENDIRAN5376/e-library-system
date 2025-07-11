700c21c82845afa9317af12ded8aebd93dd17a75
Gajendiran <kgaja560@gmail.com>
2025-07-11 10:32:29 -0700

    Create models.py

---
+++ backend/books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    is_available = models.BooleanField(default=True)
+
+class BorrowRecord(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    returned_at = models.DateTimeField(null=True, blank=True)
